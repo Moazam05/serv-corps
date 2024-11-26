4da1ad85001ce4f6fa91c3dd32e6e0363bf06504
Moazam05 <salmanmoazam08@gmail.com>
2024-11-26 15:28:41 +0500

    store setup

---
+++ src/redux/api/apiSlice.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const apiSlice = createApi({
+  reducerPath: "Api",
+
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_REACT_APP_API_URL,
+    prepareHeaders: (headers, { getState }) => {
+      const token =
+        getState().auth?.user?.token || localStorage.getItem("mp-user-token");
+      if (token) {
+        headers.set("authorization", `Token ${token}`);
+        headers.set("Content-Type", "application/json");
+        headers.set("Accept", "application/json");
+      }
+      return headers;
+    },
+  }),
+  tagTypes: [], // Define tag types as needed
+  endpoints: () => ({}), // Define endpoints as needed
+});
